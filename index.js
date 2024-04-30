221a05758bc3b9503bbab9642d82abcb1330ef92
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-04-30 15:37:18 +0600

    delete operation done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -28,10 +28,30 @@ async function run() {
 
     const addListCollection = client.db("TouristSpotListDB").collection("TouristSpotList")
 
+    app.get('/addList', async (req, res) => { 
+      const cursor = addListCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/addList/:id', async (req, res) => { 
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await addListCollection.findOne(query);
+      res.send(result);
+    })
+
     app.post('/addList', async (req, res) => {
       const addList = req.body;
       console.log(addList);
       const result = await addListCollection.insertOne(addList);
+      res.send(result);
+    })
+
+    app.delete('/addList/:id', async (req, res) => { 
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await addListCollection.deleteOne(query);
       res.send(result);
     })
 
